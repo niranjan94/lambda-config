ce27769a091c6650a0fa9f6dd86cf7ad5ae5e093
Niranjan Rajendran <me@niranjan.io>
2019-09-23 16:10:03 +0530

    Support retriving config as string or file from SSM

--- lambda_config/config.py
+++ lambda_config/config.py
@@ -1,5 +1,6 @@
 import json
 import os
+import tempfile
 import traceback
 
 import boto3
@@ -18,7 +19,7 @@ def load_config() -> Dict:
     """
     Loads config from SSM parameter store and parses it based on the set type.
 
-    :return:
+    :return: the config as dictionary
     """
     if ssm_path != '':
         return load_config_for_ssm_path(ssm_path)
@@ -27,13 +28,35 @@ def load_config() -> Dict:
     return {}
 
 
-def load_config_for_ssm_path(path: str) -> Dict:
+def get_config_from_ssm_path(path: str, store: bool = False) -> str:
+    """
+    Get config from the given SSM Parameter store path.
 
+    :param str path: path to the config
+    :param bool store: should the config be stored to a file
+    :return: config as string or path to config as string.
+    """
+    config_file_path = tempfile.mktemp()
+    client = boto3.client('ssm')
+    config_data = client.get_parameter(Name=path, WithDecryption=True)['Parameter']['Value']
+    if not store:
+        return config_data
+    with open(config_file_path, 'w') as fd:
+        fd.write(config_data)
+    return config_file_path
+
+
+def load_config_for_ssm_path(path: str) -> Dict:
+    """
+    Load config from an SSM Parameter store path.
+
+    :param str path: path to the config
+    :return: the config as dictionary
+    """
     resolved_path = os.path.expandvars(path)
     try:
-        client = boto3.client('ssm')
-        config_data = client.get_parameter(Name=resolved_path, WithDecryption=True)['Parameter']['Value']
-    except:
+        config_data = get_config_from_ssm_path(resolved_path)
+    except Exception:
         print(f'failed to load config from SSM path {resolved_path}')
         traceback.print_exc()
         return {}
@@ -58,8 +81,8 @@ def load_config_from_yaml(config_data: str) -> Dict:
     """
     Parses a yaml config string into a dict.
 
-    :param config_data:
-    :return:
+    :param str config_data: the config data to parse
+    :return: the parsed config as dictionary
     """
     return yaml.load(config_data, Loader=yaml.FullLoader)
 
@@ -68,8 +91,8 @@ def load_config_from_json(config_data: str) -> Dict:
     """
     Parses a json config string into a dict.
 
-    :param config_data:
-    :return:
+    :param str config_data: the config data to parse
+    :return: the parsed config as dictionary
     """
     return json.loads(config_data)
 
@@ -78,8 +101,8 @@ def load_config_from_ini(config_data: str) -> Dict:
     """
     Parses an init config string into a dict.
 
-    :param config_data:
-    :return:
+    :param str config_data: the config data to parse
+    :return: the parsed config as dictionary
     """
     config_dict = {}
     config = RawConfigParser(allow_no_value=True)
--- lambda_config/utils.py
+++ lambda_config/utils.py
@@ -4,6 +4,13 @@ from typing import Dict, List
 
 
 def dict_merge(destination: Dict, changes_list: List[Dict]):
+    """
+    Recursively merge dictionaries.
+
+    :param dict destination: the target dictionary
+    :param List[Dict] changes_list: the source dictionaries
+    :return: the merged dictionary
+    """
     for changes in changes_list:
         for k, v in changes.items():
             if k in destination and isinstance(destination[k], dict) and isinstance(changes[k], collections.Mapping):
